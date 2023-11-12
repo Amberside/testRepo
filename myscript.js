9d7ae46aa58bd41bd77c3b9f8e56b3efa4021d1f
chopsta-dex <146788748+chopsta-dex@users.noreply.github.com>
2023-11-13 09:52:08 +1100

    Update myscript.js
    i made it better

--- myscript.js
+++ myscript.js
@@ -1,3 +1,6 @@
 function displayMessage(){
   document.getElementById("message-box").innerHTML += "Hello GitHub!<br/>";
 }
+
+function betterMessage() {
+  document.getElementById("message-box-2".innerHTML += "What's up nerds, it's coding time &#128526 <br/>";
